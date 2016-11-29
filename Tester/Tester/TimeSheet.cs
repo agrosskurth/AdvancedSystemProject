9c1e1b3b87bef8ee030d888a676e27d0f00b37dc
Jake-Steel <r.j.hodge@gmail.com>
2016-11-29 16:52:33 -0500

    Changes to Tester

--- Tester/Tester/TimeSheet.cs
+++ Tester/Tester/TimeSheet.cs
@@ -45,7 +45,7 @@ namespace Tester
 
                 while (dr.Read())
                 {
-                    TimeIO t1 = new TimeIO(((String)dr.GetValue(1)), (DateTime)dr.GetValue(2), (DateTime)dr.GetValue(3), (String)dr.GetValue(4), (int)dr.GetValue(0));
+                    TimeIO t1 = new TimeIO(((String)dr.GetValue(1)), (DateTime)dr.GetValue(2), (DateTime)dr.GetValue(3), (String)dr.GetValue(4), (int)dr.GetValue(0), (bool)dr.GetValue(5));
                     getTS().Add(t1);
                 }
             }
